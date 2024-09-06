0d085bf19b0b16b4dd7102224ac6a7fcae85b566
Bonechka81 <asanova81.06@mail.ru>
2024-09-06 16:58:15 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,11 @@
+print(12 + 89 - 1)
+# 1st programm
+print((9 ** 0.5)* 5)
+# 2st programm
+print((9.99 > 9.98) and (1000 != 1000.1))
+#3st programm
+print(((1234 % 1000)// 10) + ((5678%1000)//10))
+#4st programm
+print((13.42 * 100)// 100 == ((42.13 * 100)% 100))
+
+
